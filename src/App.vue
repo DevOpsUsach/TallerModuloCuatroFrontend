9bc0deaa6de34c58d9a08e829153630fbc924fa3
Marcelo Leiva <mleiva@greenbot.cl>
2022-03-28 14:12:55 -0300

    Add ids

--- src/App.vue
+++ src/App.vue
@@ -87,11 +87,11 @@
           <tbody>
             <tr>
               <td style="width: 60%">Fecha</td>
-              <td class="text-end" id="fila-resultado-dxc">{{ uf.fecha }}</td>
+              <td class="text-end" id="fila-resultado-uf-fecha">{{ uf.fecha }}</td>
             </tr>
             <tr>
               <td scope="row" style="width: 60%">Valor</td>
-              <td class="text-end" id="fila-resultado-saldo">
+              <td class="text-end" id="fila-resultado-uf-valor">
                 ${{ uf.valor }}
               </td>
             </tr>
